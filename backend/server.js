68a76c6124c078b7f491a9b95c8845987ccc8c30
Kamil <kamil.bartko1@gmail.com>
2025-10-04 22:10:33 +0200

    Zmeny spat k povodnemu kodu

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3000;
-
+// skuska pridat nieco do kodu)
 // --- pre __dirname (v ES modules) ---
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
